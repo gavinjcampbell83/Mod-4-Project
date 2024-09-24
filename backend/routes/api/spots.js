f468b7933d4e19581aa5fa260e7742f8309d0d26
gavinjcampbell83 <gavinjcampbell83@gmail.com>
2024-09-24 11:26:00 -0600

    edit a spot and delete a spot working

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -48,9 +48,9 @@ router.post('/', requireAuth, async (req, res, next) => {
         if (!country) err.errors.country = "Country is required";
         if (lat < -90 || lat > 90) err.errors.lat = "Latitude must be within -90 and 90";
         if (lng < -180 || lng > 180) err.errors.lng = "Longitude must be within -180 and 180";
-        if (name.length > 50) err.errors.name = "Name must be less than 50 characters";
+        if (!name || name.length > 50) err.errors.name = "Name must be less than 50 characters";
         if (!description) err.errors.description = "Description is required";
-        if (price <= 0) err.errors.price = "Price per day must be a positive number";
+        if (!price || price <= 0) err.errors.price = "Price per day must be a positive number";
         return next(err);
     }    
         const createSpot = await Spots.create({
@@ -59,5 +59,68 @@ router.post('/', requireAuth, async (req, res, next) => {
         res.status(201).json({ createSpot });
     
 });
+
+router.put('/:spotId', requireAuth, async (req, res, next) => {
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+
+    const spotId = parseInt(req.params.spotId);
+    // console.log(spotId);
+
+    const updatedSpot = await Spots.findByPk(spotId);
+    // console.log(updatedSpot);
+
+    if(!updatedSpot){
+        return res.status(404).json({
+            "message": "Spot couldn't be found"
+          })
+    }
+
+    if (!address || !city || !state || !country || !lat || !lng || !name || !description || !price) {
+        const err = new Error("Bad Request");
+        err.status = 400; 
+        err.errors = {};
+        if (!address) err.errors.address = "Street address is required";
+        if (!city) err.errors.city = "City is required";
+        if (!state) err.errors.state = "State is required";
+        if (!country) err.errors.country = "Country is required";
+        if (lat < -90 || lat > 90) err.errors.lat = "Latitude must be within -90 and 90";
+        if (lng < -180 || lng > 180) err.errors.lng = "Longitude must be within -180 and 180";
+        if (!name || name.length > 50) err.errors.name = "Name must be less than 50 characters";
+        if (!description) err.errors.description = "Description is required";
+        if (!price || price <= 0) err.errors.price = "Price per day must be a positive number";
+        return next(err);
+    } 
+
+    await updatedSpot.update({
+        address, 
+        city, 
+        state, 
+        country, 
+        lat, 
+        lng, 
+        name, 
+        description, 
+        price
+    })
+    return res.status(200).json(updatedSpot)
+})
+
+
+router.delete('/:spotId', requireAuth, async (req, res, next) => {
+    const spotId = parseInt(req.params.spotId);
+    const deletedSpot = await Spots.findByPk(spotId);
+
+    if(!deletedSpot){
+        return res.status(404).json({
+            "message": "Spot couldn't be found"
+          })
+    }
+
+    await deletedSpot.destroy();
+    res.status(200).json({
+        "message": "Successfully deleted"
+      })
+})
+
    
 module.exports = router;
