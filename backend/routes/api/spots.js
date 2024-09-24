9162898b0d899054a1c3eca4fbb9473f29ed0ea8
gavinjcampbell83 <gavinjcampbell83@gmail.com>
2024-09-24 08:46:34 -0600

    cleaned up get all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const { Spots } = require('../../db/models')
 const { check } = require('express-validator');
@@ -9,17 +9,7 @@ const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 // //get all spots
-// router.get('/', async (req, res) => {
-//     console.log('can i make it?')
-//     const spot = await Spot.findAll({
-//     // include: [{ model: Review.avgreview}, { model: image.url}]
-    
-//     }) 
-//     res.status(200).json({ Spots: spot })
-// });
-
 router.get('/', async (req, res) => {
-    console.log('Hello World')
     const spots = await Spots.findAll();
     res.status(200).json({ Spots: spots });
 });
