2469ce33a6347d91dbed940700f54919be8b781c
Quonnaq0711 <Shaquonna.lynna@gmail.com>
2024-09-25 22:21:56 -0400

    <reviewpost1>

--- backend/routes/api/review.js
+++ backend/routes/api/review.js
@@ -10,13 +10,9 @@ const router = express.Router();
 
 //Get all Reviews of the Current User
 router.get('/current', requireAuth, async (req, res) => {
-    
-        console.log('made it here');
-        const currentUser = req.user.username;
-        console.log('made it here');
-        
-        const userReviews = await Review.findAll({           
-            include: [
+    const currentUser = req.user.username;
+    const userReviews = await Review.findAll({
+        include: [
                 {
                     model: User,
                     attributes: { exclude: ['createdAt', 'updatedAt', 'username', 'email', 'hashedPassword'] }
@@ -31,9 +27,51 @@ router.get('/current', requireAuth, async (req, res) => {
                 }
             ]  
         });
-
         res.status(200).json({ Reviews: userReviews });
+});
+
+//Create a Review for a Spot based on the Spot's id
+router.post('/spots/:spotId/reviews', requireAuth, async (req, res, next) => {
+    const { review, stars } = req.body;
+    const { spotId } = req.params;
+    const userId = req.user.id;
+    console.log("did it agin")
+
+    if (!review || !stars) {
+        const err = new Error("Bad Request");
+        err.status = 400;
+        err.errors = {};
+        if (!review) err.errors.review = "Review text is required";
+        if (!stars) err.errors.stars = "Stars must be an integer from 1 to 5";
+        return next(err);
+    }
+console.log("did it agin1")
+
+
+    const spot = await Spots.findByPk(spotId);
+    if (!spot) {
+        return res.status(404).json({
+        "message": "Spot couldn't be found"
+        });
+    }
+    console.log("did it agin2")
+
+    const existingReview = await Review.findOne({
+        where: { spotId, userId }
     });
+
+    if (existingReview) {
+        return res.status(500).json({
+        "message": "User already has a review for this spot"
+        });
+    }
+console.log("did it agin3")
+
+    const createReview = await Review.create({
+        review, stars               
+    });
+    res.status(201).json(createReview);    
+});
   
 
 
